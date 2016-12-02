b44c5938ca4ceca4d60cc8e55b10344033d91b02
Zhichuang Sun <zhisun@cs.stonybrook.edu>
2016-12-02 00:56:00 -0500

    add shuffle for train data and add range selection for train data
    Signed-off-by: Zhichuang Sun <zhisun@cs.stonybrook.edu>

--- classifier.py
+++ classifier.py
@@ -4,6 +4,7 @@
 # Date: 2016.11.30
 
 import os
+import random
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
@@ -46,19 +47,27 @@ class Classifier:
 				#content = '\n'.join((f.read().encode('utf-8')).split('\n'[5:]))
 				data.append(content)
 				target.append(idx)
-		return data, target
+		shuffled_idx = list(range(len(data)))
+		random.shuffle(shuffled_idx)
+		shuffled_data = []
+		shuffled_target = []
+		for i in shuffled_idx:
+			shuffled_data.append(data[i])
+			shuffled_target.append(target[i])
+		return shuffled_data, shuffled_target
 			
 	def escape_header(self, file_content):
 		# escape the header (first 5 lines)
 		return '\n'.join(file_content.split('\n')[5:])
 
-	def train_nb(self):
+	def train_nb(self, percent):
 		self.unigram_count_vect = CountVectorizer()
 		self.bigram_count_vect = CountVectorizer(ngram_range=(2, 2))
-		X_train_counts_u = self.unigram_count_vect.fit_transform(self.train_data)
-		X_train_counts_b = self.bigram_count_vect.fit_transform(self.train_data)
-		self.uclf = MultinomialNB().fit(X_train_counts_u, self.train_target)
-		self.bclf = MultinomialNB().fit(X_train_counts_b, self.train_target)
+		bound = int(len(self.train_data)/100) * percent
+		X_u = self.unigram_count_vect.fit_transform(self.train_data[:bound])
+		X_b = self.bigram_count_vect.fit_transform(self.train_data[:bound])
+		self.uclf = MultinomialNB().fit(X_u, self.train_target[:bound])
+		self.bclf = MultinomialNB().fit(X_b, self.train_target[:bound])
 
 	def get_result(self):
 		X_test_counts_u = self.unigram_count_vect.transform(self.test_data)
@@ -75,38 +84,47 @@ class Classifier:
 		print(metrics.recall_score(self.test_target, upredicted, average='macro'))
 		print(metrics.f1_score(self.test_target, bpredicted, average='macro'))
 
-	def train_svm(self):
+	def train_svm(self, percent):
 		self.unigram_count_vect = CountVectorizer()
 		self.bigram_count_vect = CountVectorizer(ngram_range=(2, 2))
-		X_train_counts_u = self.unigram_count_vect.fit_transform(self.train_data)
-		X_train_counts_b = self.bigram_count_vect.fit_transform(self.train_data)
-		self.uclf = SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter=5, random_state=42).fit(X_train_counts_u, self.train_target)
-		self.bclf = SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter=5, random_state=42).fit(X_train_counts_b, self.train_target)
+		bound = int(len(self.train_data)/100) * percent
+		X_u = self.unigram_count_vect.fit_transform(self.train_data[:bound])
+		X_b = self.bigram_count_vect.fit_transform(self.train_data[:bound])
+		#X_train_counts_u = self.unigram_count_vect.fit_transform(self.train_data)
+		#X_train_counts_b = self.bigram_count_vect.fit_transform(self.train_data)
+		self.uclf = SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter=5, random_state=42).fit(X_u, self.train_target[:bound])
+		self.bclf = SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter=5, random_state=42).fit(X_b, self.train_target[:bound])
 
-	def train_lr(self):
+	def train_lr(self, percent):
 		self.unigram_count_vect = CountVectorizer()
 		self.bigram_count_vect = CountVectorizer(ngram_range=(2, 2))
-		X_u = self.unigram_count_vect.fit_transform(self.train_data)
-		X_b = self.bigram_count_vect.fit_transform(self.train_data)
-		self.uclf = LogisticRegression(C = 100, penalty='l2',tol = 0.01).fit(X_u, self.train_target)
-		self.bclf = LogisticRegression(C = 100, penalty='l2',tol = 0.01).fit(X_b, self.train_target)
+		bound = int(len(self.train_data)/100) * percent
+		X_u = self.unigram_count_vect.fit_transform(self.train_data[:bound])
+		X_b = self.bigram_count_vect.fit_transform(self.train_data[:bound])
+		#X_u = self.unigram_count_vect.fit_transform(self.train_data)
+		#X_b = self.bigram_count_vect.fit_transform(self.train_data)
+		self.uclf = LogisticRegression(C = 100, penalty='l2',tol = 0.01).fit(X_u, self.train_target[:bound])
+		self.bclf = LogisticRegression(C = 100, penalty='l2',tol = 0.01).fit(X_b, self.train_target[:bound])
 
-	def train_rf(self):
+	def train_rf(self, percent):
 		self.unigram_count_vect = CountVectorizer()
 		self.bigram_count_vect = CountVectorizer(ngram_range=(2, 2))
-		X_u = self.unigram_count_vect.fit_transform(self.train_data)
-		X_b = self.bigram_count_vect.fit_transform(self.train_data)
-		self.uclf = RandomForestClassifier(n_estimators=10, max_depth=None,min_samples_split=2, random_state=0).fit(X_u, self.train_target)
-		self.bclf = RandomForestClassifier(n_estimators=10, max_depth=None,min_samples_split=2, random_state=0).fit(X_b, self.train_target)
+		bound = int(len(self.train_data)/100) * percent
+		X_u = self.unigram_count_vect.fit_transform(self.train_data[:bound])
+		X_b = self.bigram_count_vect.fit_transform(self.train_data[:bound])
+		#X_u = self.unigram_count_vect.fit_transform(self.train_data)
+		#X_b = self.bigram_count_vect.fit_transform(self.train_data)
+		self.uclf = RandomForestClassifier(n_estimators=10, max_depth=None,min_samples_split=2, random_state=0).fit(X_u, self.train_target[:bound])
+		self.bclf = RandomForestClassifier(n_estimators=10, max_depth=None,min_samples_split=2, random_state=0).fit(X_b, self.train_target[:bound])
 
 if __name__ == '__main__':
 	categories = ['rec.sport.hockey','sci.med','soc.religion.christian','talk.religion.misc']
 	cla = Classifier(categories)
-	cla.train_nb()
+	cla.train_nb(100)
 	cla.get_result()
-	cla.train_svm()
+	cla.train_svm(100)
 	cla.get_result()
-	cla.train_lr()
+	cla.train_lr(100)
 	cla.get_result()
-	cla.train_rf()
+	cla.train_rf(100)
 	cla.get_result()
